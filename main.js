5b52a011e4894b026bb48334a7e1745b153fc66f
Berke Soysal <soysalberke.96@gmail.com>
2024-01-21 10:54:51 +0100

    use let, const

--- main.js
+++ main.js
@@ -1,37 +1,37 @@
-var holidays = [
+const holidays = [
 	'2024-01-01', '2024-04-10', '2024-04-11', '2024-04-12', '2024-04-23', '2024-05-01', '2024-05-19', '2024-06-15', '2024-06-16'
     , '2024-06-17', '2024-06-18', '2024-06-19', '2024-07-15', '2024-08-30', '2024-10-29',
 ];
 
-var earlyCloseDates = [
+const earlyCloseDates = [
 	'2024-04-09', '2024-10-28',
 ];
 
-function borsaStatus(currentDateISOString) {
+function bistStatus(currentDateISOString) {
 
-    var now = moment(currentDateISOString).tz('Europe/Istanbul');
-
+    let now = moment(currentDateISOString).tz('Europe/Istanbul');
+    let timeToWait = 0;
     if (!isWorkingDay(now)) {
-        var nextOpenDate = nextWorkingDay(now).hour(10).minute(0).second(0);
-        var timeToWait = moment.duration(nextOpenDate.diff(now, 'seconds'), 'seconds');
+        let nextOpenDate = nextWorkingDay(now).hour(10).minute(0).second(0);
+        timeToWait = moment.duration(nextOpenDate.diff(now, 'seconds'), 'seconds');
         return {currentStatus: 'KAPALI', nextStatus: 'AÇIK', timeUntilNextStatus: durationToString(timeToWait)};
     }
 
     var openDate = moment(now).hour(10).minute(0).second(0);
     if (now.isBefore(openDate)) {
-        var timeToWait = moment.duration(openDate.diff(now, 'seconds'), 'seconds');
+        timeToWait = moment.duration(openDate.diff(now, 'seconds'), 'seconds');
         return {currentStatus: 'KAPALI', nextStatus: 'AÇIK', timeUntilNextStatus: durationToString(timeToWait)};
     }
 
-    var closingHour = isEarlyClose(now)? 13 : 18;
-    var closeDate = moment(now).hour(closingHour).minute(0).second(0);
+    let closingHour = isEarlyClose(now)? 13 : 18;
+    let closeDate = moment(now).hour(closingHour).minute(0).second(0);
     if (now.isAfter(closeDate)) {
         var nextOpenDate = nextWorkingDay(now).hour(10).minute(0).second(0);
-        var timeToWait = moment.duration(nextOpenDate.diff(now, 'seconds'), 'seconds');
+        timeToWait = moment.duration(nextOpenDate.diff(now, 'seconds'), 'seconds');
         return {currentStatus: 'KAPALI', nextStatus: 'AÇIK', timeUntilNextStatus: durationToString(timeToWait)};
     }
 
-    var timeToWait = moment.duration(closeDate.diff(now, 'seconds'), 'seconds');
+    timeToWait = moment.duration(closeDate.diff(now, 'seconds'), 'seconds');
     return {currentStatus: 'AÇIK', nextStatus: 'KAPALI', timeUntilNextStatus: durationToString(timeToWait)};
 }
 
@@ -39,12 +39,12 @@ function isWorkingDay(now) {
 	if (isHoliday(now)) return false;
     var SUNDAY = 0;
     var SATURDAY = 6;
-    return !(now.day() == SUNDAY || now.day() == SATURDAY);
+    return !(now.day() === SUNDAY || now.day() === SATURDAY);
 }
 
 function isHoliday(now) {
 	for (var i = 0; i < holidays.length; i++) {
-		var holiday = moment.tz(holidays[i], "Europe/Istanbul");
+		let holiday = moment.tz(holidays[i], "Europe/Istanbul");
 		if (now.isSame(holiday, 'day')) {
 			return true;
 		}
@@ -54,7 +54,7 @@ function isHoliday(now) {
 
 function isEarlyClose(now) {
 	for (var i = 0; i < earlyCloseDates.length; i++) {
-		var earlyCloseDate = moment.tz(earlyCloseDates[i], "Europe/Istanbul");
+		let earlyCloseDate = moment.tz(earlyCloseDates[i], "Europe/Istanbul");
 		if (now.isSame(earlyCloseDate, 'day')) {
 			return true;
 		}
@@ -63,7 +63,7 @@ function isEarlyClose(now) {
 }
 
 function nextWorkingDay(now) {
-    var candidate = moment(now).add(1, 'days');
+    let candidate = moment(now).add(1, 'days');
     while(!isWorkingDay(candidate))
         candidate = candidate.add(1, 'days');
     return candidate;
@@ -74,7 +74,7 @@ function durationToString(duration) {
 }
 
 function myTimer() {
-    var status = borsaStatus();
+    var status = bistStatus();
     $('#status').html(status.currentStatus);
     $('#status').addClass(status.currentStatus.toLowerCase());
     $('#nextStatus').html(status.nextStatus);
